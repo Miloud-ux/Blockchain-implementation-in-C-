1b8093b682154f65a5e9369de0996cf3bceab7a9
Miloud <basicalllymiloud@gmail.com>
2026-05-25 11:09:30 +0100

    Update project.c

--- project.c
+++ project.c
@@ -15,7 +15,7 @@
 #define MAX_HASH_SIZE 65
 #define MAX_CONCATINATED_SIZE 10000
 #define MAX_USERNAME_SIZE 50
-#define MAX_TX 10 // maximum transactions
+#define MAX_TX 10
 
 typedef struct Transaction {
   char sender[MAX_USERNAME_SIZE];
@@ -24,34 +24,25 @@ typedef struct Transaction {
 } Transaction;
 
 typedef struct Block {
-  int index; // the height of the block
+  int index;
   char timestamps[20];
   Transaction transactions[MAX_TX];
-  int tx_count; // count how many transactions are in this Block.
+  int tx_count;
   char current_hash[MAX_HASH_SIZE];
   char prev_hash[MAX_HASH_SIZE];
   int nonce;
   struct Block *next_block;
 } Block;
 
-// Transactions
 void add_transaction(Block *block, char *from, char *to, int amount);
-
-// Block mining
 void mine_block(Block *block);
-void mine_latest_block(Block *block); // For easier testing
-
-// Output :
+void mine_latest_block(Block *block);
 void print_chain(Block *genesis_block);
 void spinning_loading_minning();
-
-// Extra
 void spinning_loading();
 void spinning_loading_util(char spin_char);
-
-// Block structure
 void free_chain(Block **genesis_block);
-void test_invalid_chain(Block *genesis_block); // for debugging
+void test_invalid_chain(Block *genesis_block);
 bool validate_block_chain(Block **block);
 void delete_invalid_block(Block **genesis_block);
 
@@ -66,9 +57,7 @@ void hash_func(Block *myblock) {
     return;
   }
 
-  // Concatinate all the fields in a single string
   char con_data[MAX_CONCATINATED_SIZE];
-
   int char_written =
       snprintf(con_data, sizeof(con_data), "%d%s%s%d", myblock->index,
                myblock->timestamps, myblock->prev_hash, myblock->nonce);
@@ -78,10 +67,8 @@ void hash_func(Block *myblock) {
     exit(1);
   }
 
-  // Concatinate all the transactions to hash them
   for (int i = 0; i < myblock->tx_count; i++) {
     int remaining = MAX_CONCATINATED_SIZE - char_written;
-
     int tx_written = snprintf(con_data + char_written, remaining, "%s%s%d",
                               myblock->transactions[i].sender,
                               myblock->transactions[i].receiver,
@@ -91,43 +78,32 @@ void hash_func(Block *myblock) {
       printf("Transaction Error : Buffer overflow");
       exit(1);
     }
-
     char_written += tx_written;
   }
 
-  // Hashing
   unsigned char digest[SHA256_DIGEST_LENGTH];
   SHA256((unsigned char *)con_data, strlen(con_data), digest);
 
-  // Convert the raw hash into hexa string
   char hex[65];
   for (size_t i = 0; i < SHA256_DIGEST_LENGTH; i++) {
     sprintf(hex + (i * 2), "%02x", digest[i]);
   }
   hex[64] = '\0';
 
-  // copy the hash into the block
   strcpy(myblock->current_hash, hex);
 }
 
 void init_new_block(Block *genesis_block) {
   set_time_stamps(genesis_block);
-
   genesis_block->index = 0;
   genesis_block->tx_count = 0;
-  genesis_block->next_block = NULL; // 0 = NULL
-
+  genesis_block->next_block = NULL;
   strcpy(genesis_block->prev_hash, "0");
   strcpy(genesis_block->current_hash, "0");
-
   memset(genesis_block->transactions, 0, sizeof(genesis_block->transactions));
-  // strcpy(genesis_block->data, "0");
 }
 
 void create_new_block(Block **genesis_block) {
-  // we only need the data to be passed as a parameter
-  // Block** pass the genensis block by reference since it's local to the main
-
   if (*genesis_block == NULL) {
     perror("The genesis block doesn't exist");
     return;
@@ -139,17 +115,15 @@ void create_new_block(Block **genesis_block) {
     temp = temp->next_block;
     index++;
   }
-  // temp is at the end of the list
-  // we create our block and initialize.
+
   Block *new_block = (Block *)malloc(sizeof(Block));
   if (!new_block) {
     perror("Error Allocating data for the new block");
     exit(EXIT_FAILURE);
   }
-  init_new_block(new_block); // to avoid garbage values
+  init_new_block(new_block);
   new_block->index = index + 1;
 
-  // now link the blocks and fill the "prev hash" field
   strcpy(new_block->prev_hash, temp->current_hash);
   temp->next_block = new_block;
 }
@@ -159,7 +133,7 @@ void create_new_block_wrapper(Block **genesis_block) {
     system("clear");
     char answer;
     printf("Do you want to Create a new block  ? (Y/n)");
-    scanf(" %c", &answer); // leave space to clear the buffer
+    scanf(" %c", &answer);
     if (tolower(answer) == 'n') {
       printf("Have a good day");
       return;
@@ -170,7 +144,6 @@ void create_new_block_wrapper(Block **genesis_block) {
       create_new_block(genesis_block);
       if (!validate_block_chain(genesis_block)) {
         printf("New block invalid !");
-        // Add an ASCII spinner
       }
       delete_invalid_block(genesis_block);
       printf("Done");
@@ -180,8 +153,8 @@ void create_new_block_wrapper(Block **genesis_block) {
     }
   }
 }
+
 int main() {
-  // Create the first block (Genesis block)
   Block *genesis_block;
   genesis_block = malloc(sizeof(Block));
   if (!genesis_block) {
@@ -189,29 +162,16 @@ int main() {
     return 1;
   }
 
-  // init the genesis block
   init_new_block(genesis_block);
-
-  // Assign the hash for the genesis block
   hash_func(genesis_block);
 
-  // Interactive block creation:
   test_invalid_chain(genesis_block);
   free_chain(&genesis_block);
-  // create_new_block_wrapper(&genesis_block);
-
-  // free the whole chain
 
   return 0;
 }
 
 bool validate_block_chain(Block **genesis_block) {
-  /* 1. Recompute the hash.
-   *  2. Check : curr -> hash = prev -> hash
-   *  3. Check : curr -> index = prev -> index + 1
-   *  4. Verify the PoW.
-   */
-
   if (*genesis_block == NULL) {
     return false;
   }
@@ -249,7 +209,6 @@ bool validate_block_chain(Block **genesis_block) {
     curr = curr->next_block;
   }
 
-  // We still need to check for the latest block:
   for (int i = 0; i < MINNING_DIFFUCULTY; i++) {
     if (curr->current_hash[i] != '0') {
       fprintf(stderr, "Block Error : Not matching the correct PoW difficulty");
@@ -261,14 +220,10 @@ bool validate_block_chain(Block **genesis_block) {
 }
 
 void delete_invalid_block(Block **genesis_block) {
-  // 1. Loop through the linked list till the block before the end
-  // 2. Set the next_block == NULL
   if (genesis_block == NULL || (*genesis_block)->next_block == NULL) {
-    // nothing to delete here
     return;
   }
 
-  // Case 2 : More than two blocks
   Block *temp = *genesis_block;
   while (temp->next_block->next_block != NULL) {
     temp = temp->next_block;
@@ -278,20 +233,20 @@ void delete_invalid_block(Block **genesis_block) {
 }
 
 void spinning_loading() {
-  // const char *theme_1 = "⣾⣽⣻⢿⡿⣟⣯⣷";
   const char *theme_1 = "|/-\\|/-\\";
-  for (int cycle = 0; cycle < 2; cycle++) { // 3 rotations
-    for (unsigned short int i = 0; i < 4;
-         i++) { // experimenting with different data types  :)
+  for (int cycle = 0; cycle < 2; cycle++) {
+    for (unsigned short int i = 0; i < 4; i++) {
       spinning_loading_util(theme_1[i]);
     }
   }
 }
+
 void spinning_loading_util(char spin_char) {
   printf("\r%c ", spin_char);
   fflush(stdout);
   usleep(200000);
 }
+
 void test_invalid_chain(Block *genesis_block) {
   Block *chain = genesis_block;
 
@@ -330,11 +285,9 @@ void test_invalid_chain(Block *genesis_block) {
 
   print_chain(genesis_block);
 
-  // Test 1: Corrupt hash
   strcpy(chain->next_block->current_hash, "0000000000");
   assert(validate_block_chain(&chain) == false);
 
-  // Test 2: Break link
   strcpy(chain->next_block->prev_hash, "broken_link");
   assert(validate_block_chain(&chain) == false);
 }
@@ -366,12 +319,10 @@ void print_chain(Block *genesis_block) {
 
   Block *curr = genesis_block;
   while (curr != NULL) {
-    // Use your spinning loader
     printf("    Loading block %d ", curr->index);
     spinning_loading();
     printf(" ✓ \n");
 
-    // Print block info
     if (curr->index == 0) {
       printf(" -------- GENESIS BLOCK --------\n");
     } else {
@@ -391,11 +342,10 @@ void print_chain(Block *genesis_block) {
     }
     printf("------------------------------\n");
 
-    // Connection arrow to next block (if not last)
     if (curr->next_block != NULL) {
       printf("              |\n");
       printf("              ↓\n");
-      usleep(300000); // 0.3 seconds pause
+      usleep(300000);
     }
 
     printf("\n");
@@ -407,12 +357,10 @@ void print_chain(Block *genesis_block) {
 }
 
 void mine_block(Block *block) {
-
   block->nonce = 0;
   printf("Mining Block : %d...\n", block->index);
 
   while (true) {
-
     hash_func(block);
 
     bool is_valid = true;
@@ -440,13 +388,14 @@ void mine_block(Block *block) {
 }
 
 void spinning_loading_minning() {
-  static int i = 0; // persists between calls
+  static int i = 0;
   const char *theme = "|/-\\";
 
   printf("\r%c Mining... ", theme[i]);
   fflush(stdout);
 
-  i = (i + 1) % 4; // increment and wrap around (0 → 1 → 2 → 3 → 0...)
+  // keep changing the spinner character in a circle
+  i = (i + 1) % 4;
 }
 
 void add_transaction(Block *block, char *from, char *to, int amount) {
@@ -457,9 +406,7 @@ void add_transaction(Block *block, char *from, char *to, int amount) {
   }
   Transaction *tx = &block->transactions[block->tx_count];
 
-  strncpy(tx->receiver, to,
-          MAX_USERNAME_SIZE -
-              1); // so we don't run into null termination errors
+  strncpy(tx->receiver, to, MAX_USERNAME_SIZE - 1);
   tx->receiver[MAX_USERNAME_SIZE - 1] = '\0';
 
   strncpy(tx->sender, from, MAX_USERNAME_SIZE - 1);
@@ -475,8 +422,7 @@ void mine_latest_block(Block *block) {
     return;
   }
 
-  Block *temp = block; // Actually no need for temp here since the pointer
-                       // stored is just a local copy
+  Block *temp = block;
   while (temp->next_block != NULL) {
     temp = temp->next_block;
   }
